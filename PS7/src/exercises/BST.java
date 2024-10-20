786b92e9c5a27adff8937159c7c47796e6fc1b8d
Krish A. Patel <krishpatel2067@gmail.com>
2024-10-19 20:41:47 -0400

    Make half of the lines spaces -> tabs

--- PS7/src/exercises/BST.java
+++ PS7/src/exercises/BST.java
@@ -4,99 +4,99 @@ import java.util.ArrayList;
 
 /** BST implementation for Dictionary ADT */
 class BST<K extends Comparable<K>, E> implements Dictionary<K, E> {
-  private BSTNode<K, E> root; // Root of BST
-  private int nodecount; // Size of BST
+	private BSTNode<K, E> root; // Root of BST
+	private int nodecount; // Size of BST
 
-  /** Constructor */
-  BST() {
-    root = null;
-    nodecount = 0;
-  }
+	/** Constructor */
+	BST() {
+		root = null;
+		nodecount = 0;
+	}
 
-  /** Reinitialize tree */
-  public void clear() {
-    root = null;
-    nodecount = 0;
-  }
+	/** Reinitialize tree */
+	public void clear() {
+		root = null;
+		nodecount = 0;
+	}
 
-  /**
-   * Insert a record into the tree.
-   * 
-   * @param k
-   *          Key value of the record.
-   * @param e
-   *          The record to insert.
-   */
-  public void insert(K k, E e) {
-    root = inserthelp(root, k, e);
-    nodecount++;
-  }
+	/**
+	 * Insert a record into the tree.
+	 * 
+	 * @param k
+	 *          Key value of the record.
+	 * @param e
+	 *          The record to insert.
+	 */
+	public void insert(K k, E e) {
+		root = inserthelp(root, k, e);
+		nodecount++;
+	}
 
-  /**
-   * Remove a record from the tree.
-   * 
-   * @param k
-   *          Key value of record to remove.
-   * @return Record removed, or null if there is none.
-   */
-  public E remove(K k) {
-    E temp = findhelp(root, k); // find it
-    if (temp != null) {
-      root = removehelp(root, k); // remove it
-      // System.out.println("called removehelp");
-      nodecount--;
-    }
-    return temp;
-  }
+	/**
+	 * Remove a record from the tree.
+	 * 
+	 * @param k
+	 *          Key value of record to remove.
+	 * @return Record removed, or null if there is none.
+	 */
+	public E remove(K k) {
+		E temp = findhelp(root, k); // find it
+		if (temp != null) {
+			root = removehelp(root, k); // remove it
+			// System.out.println("called removehelp");
+			nodecount--;
+		}
+		return temp;
+	}
 
-  /**
-   * Remove/return root node from dictionary.
-   * 
-   * @return The record removed, null if empty.
-   */
-  public E removeAny() {
-    if (root == null)
-      return null;
-    E temp = root.element();
-    root = removehelp(root, root.key());
-    --nodecount;
-    return temp;
-  }
+	/**
+	 * Remove/return root node from dictionary.
+	 * 
+	 * @return The record removed, null if empty.
+	 */
+	public E removeAny() {
+		if (root == null)
+			return null;
+		E temp = root.element();
+		root = removehelp(root, root.key());
+		--nodecount;
+		return temp;
+	}
 
-  /**
-   * @return Record with key k, null if none.
-   * @param k
-   *          The key value to find.
-   */
-  public E find(K k) {
-    return findhelp(root, k);
-  }
+	/**
+	 * @return Record with key k, null if none.
+	 * @param k
+	 *          The key value to find.
+	 */
+	public E find(K k) {
+		return findhelp(root, k);
+	}
 
-  /** @return Number of records in dictionary. */
-  public int size() {
-    return nodecount;
-  }
+	/** @return Number of records in dictionary. */
+	public int size() {
+		return nodecount;
+	}
 
-  private E findhelp(BSTNode<K, E> rt, K k) {
-    if (rt == null)
-      return null;
-    if (rt.key().compareTo(k) > 0)
-      return findhelp(rt.left(), k);
-    else if (rt.key().compareTo(k) == 0)
-      return rt.element();
-    else
-      return findhelp(rt.right(), k);
-  }
+	private E findhelp(BSTNode<K, E> rt, K k) {
+		if (rt == null)
+			return null;
+		if (rt.key().compareTo(k) > 0)
+			return findhelp(rt.left(), k);
+		else if (rt.key().compareTo(k) == 0)
+			return rt.element();
+		else
+			return findhelp(rt.right(), k);
+	}
 
-  private BSTNode<K, E> inserthelp(BSTNode<K, E> rt, K k, E e) {
-    if (rt == null)
-      return new BSTNode<K, E>(k, e);
-    if (rt.key().compareTo(k) > 0)
-      rt.setLeft(inserthelp(rt.left(), k, e));
-    else
-      rt.setRight(inserthelp(rt.right(), k, e));
-    return rt;
-  }
+	private BSTNode<K, E> inserthelp(BSTNode<K, E> rt, K k, E e) {
+		if (rt == null)
+			return new BSTNode<K, E>(k, e);
+		if (rt.key().compareTo(k) > 0)
+			rt.setLeft(inserthelp(rt.left(), k, e));
+		else
+			rt.setRight(inserthelp(rt.right(), k, e));
+		return rt;
+	}
 
   private BSTNode<K, E> getmin(BSTNode<K, E> rt) {
     if (rt.left() == null)
