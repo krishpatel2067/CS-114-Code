57952e0b52794a1f081cf0bbc1f92015abea7d09
Krish A. Patel <krishpatel2067@gmail.com>
2024-10-28 09:43:28 -0400

    Figure out which sort number is which type
    And a bunch of other stuff that I forgot to commit

--- Lab8/src/lab8/Experiment.java
+++ Lab8/src/lab8/Experiment.java
@@ -1,4 +1,5 @@
 package lab8;
+
 import java.util.ArrayList;
 import java.text.NumberFormat;
 
@@ -11,80 +12,131 @@ public class Experiment {
 
 	public static void main(String[] args) {
 
-        int s=0; // try sort(s,.)
-        testSort(0);
-        testSort(1);
-        testSort(2);
-        testSort(3);
-    }
-	
-	public static void testSort(int s)
-	{
+		int s = 0; // try sort(s,.)
+		testSort(0); 			// heap sort (rule of elimination)
+		testSort(1); 			// insertion sort (shuffled takes MUCH longer)
+        testSort(3);			// selection sort (way slower than #0 even for sorted; sorted times growing by 4x every 2x input size)
+        testSort(2);			// quick sort (instant stack overflow!)
+		System.out.println("Finished");
+	}
+
+	public static void testSort(int s) {
+		System.out.println("Testing sort " + s);
 		MysterySorts srts = new MysterySorts();
 		long start, now, elapsed;
 		int n;
 		int n0 = 10000;
 		Integer[] A;
-		
-		System.out.println("Testing sort "+s);
-        n =  n0;
-        A = new Integer[n];
 
-        // first try already sorted
-        start = System.currentTimeMillis();
-        for (int rep = 0; rep < REP; ++rep) {
-            for (int j = 0; j < n; ++j) A[j] = j;
-            srts.sort(s,A);
-        }
-        now = System.currentTimeMillis();
-        elapsed = now - start;
-        System.out.println("Sorted short array time: "+elapsed);
-        System.out.println("Sorted short array avg time: "+ (double)elapsed/REP);
+		// test sorted array over many array sizes
+		System.out.println("Sorted");
+		for (int epoch = 1; epoch <= 5; epoch++) {
+			n = n0;
+			n *= epoch;
+			A = new Integer[n];
 
-        // now do longer arrays
-        n =  2*n0;
-        A = new Integer[n];
+			start = System.currentTimeMillis();
+			for (int rep = 0; rep < REP; ++rep) {
+				for (int j = 0; j < n; ++j)
+					A[j] = j;
+				srts.sort(s, A);
+			}
+			now = System.currentTimeMillis();
+			elapsed = now - start;
+			System.out.println("\t[n = " + n + "] " + elapsed);
+			System.out.println("\t\tAvg: " + (double) elapsed / REP);
+		}
 
-        start = System.currentTimeMillis();
-        for (int rep = 0; rep < REP; ++rep) {
-            //srts.shuffleArray(A);
-            for (int j = 0; j < n; ++j) A[j] = j;
-            srts.sort(s,A);
-        }
-        now = System.currentTimeMillis();
-        elapsed = now - start;
-        System.out.println("Sorted long array time: "+elapsed);
-        System.out.println("Sorted long array avg time: "+ (double)elapsed/REP);
+		// test shuffled array over many array sizes
+		System.out.println("Shuffled");
+		for (int epoch = 1; epoch <= 5; epoch++) {
+			n = n0;
+			n *= epoch;
+			A = new Integer[n];
 
-        // now shuffled
-        n =  n0;
-        A = new Integer[n];
+			start = System.currentTimeMillis();
+			for (int rep = 0; rep < REP; ++rep) {
+				for (int j = 0; j < n; ++j)
+					A[j] = j;
+				srts.shuffleArray(A);
+				srts.sort(s, A);
+			}
+			now = System.currentTimeMillis();
+			elapsed = now - start;
+			System.out.println("\t[n = " + n + "] " + elapsed);
+			System.out.println("\t\tAvg: " + (double) elapsed / REP);
+		}
 
-        start = System.currentTimeMillis();
-        for (int rep = 0; rep < REP; ++rep) {
-            for (int j = 0; j < n; ++j) A[j] = j;
-            srts.shuffleArray(A);
-            srts.sort(s,A);
-        }
-        now = System.currentTimeMillis();
-        elapsed = now - start;
-        System.out.println("Shuffled short array time: "+elapsed);
-        System.out.println("Shuffled short array avg time: "+ (double)elapsed/REP);
+		n = n0;
+		A = new Integer[n];
 
-        // now do longer arrays
-        n =  2*n0;
-        A = new Integer[n];
+		// first try already sorted
+		start = System.currentTimeMillis();
+		for (int rep = 0; rep < REP; ++rep) {
+			for (int j = 0; j < n; ++j)
+				A[j] = j;
+			srts.sort(s, A);
+		}
+		now = System.currentTimeMillis();
+		elapsed = now - start;
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Sorted short array time: " + elapsed);
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Sorted short array avg time: " + (double) elapsed / REP);
 
-        start = System.currentTimeMillis();
-        for (int rep = 0; rep < REP; ++rep) {
-            for (int j = 0; j < n; ++j) A[j] = j;
-            srts.shuffleArray(A);
-            srts.sort(s,A);
-        }
-        now = System.currentTimeMillis();
-        elapsed = now - start;
-        System.out.println("Shuffled long array time: "+elapsed);
-        System.out.println("Shuffled long array avg time: "+ (double)elapsed/REP);
-        System.out.println();
+		// now do longer arrays
+		n = 2 * n0;
+		A = new Integer[n];
+
+		start = System.currentTimeMillis();
+		for (int rep = 0; rep < REP; ++rep) {
+			// srts.shuffleArray(A);
+			for (int j = 0; j < n; ++j)
+				A[j] = j;
+			srts.sort(s, A);
+		}
+		now = System.currentTimeMillis();
+		elapsed = now - start;
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Sorted long array time: " + elapsed);
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Sorted long array avg time: " + (double) elapsed / REP);
+
+		// now shuffled
+		n = n0;
+		A = new Integer[n];
+
+		start = System.currentTimeMillis();
+		for (int rep = 0; rep < REP; ++rep) {
+			for (int j = 0; j < n; ++j)
+				A[j] = j;
+			srts.shuffleArray(A);
+			srts.sort(s, A);
+		}
+		now = System.currentTimeMillis();
+		elapsed = now - start;
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Shuffled short array time: " + elapsed);
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Shuffled short array avg time: " + (double) elapsed / REP);
+
+		// now do longer arrays
+		n = 2 * n0;
+		A = new Integer[n];
+
+		start = System.currentTimeMillis();
+		for (int rep = 0; rep < REP; ++rep) {
+			for (int j = 0; j < n; ++j)
+				A[j] = j;
+			srts.shuffleArray(A);
+			srts.sort(s, A);
+		}
+		now = System.currentTimeMillis();
+		elapsed = now - start;
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Shuffled long array time: " + elapsed);
+//		System.out.print("[n = " + n + "] ");
+//		System.out.println("Shuffled long array avg time: " + (double) elapsed / REP);
+		System.out.println();
 	}
 }
