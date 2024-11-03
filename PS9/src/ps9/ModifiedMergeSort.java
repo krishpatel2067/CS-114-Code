b4a01815c08e8fe2ef4b8351abf4b065e0be1f8e
Krish A. Patel <krishpatel2067@gmail.com>
2024-11-03 15:56:38 -0500

    Start to reform code to divide arrays evenly

--- PS9/src/ps9/ModifiedMergeSort.java
+++ PS9/src/ps9/ModifiedMergeSort.java
@@ -3,9 +3,9 @@ package ps9;
 public class ModifiedMergeSort {
 
 	public static void main(String[] args) {
-		Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
+		Integer[] arr = {1, 2};
 		Integer[] temp = {1, 2};
-		mergeSort(arr, temp, 0, 6, 3);
+		mergeSort(arr, temp, 0, arr.length - 1, 3);
 	}
 
 	public static <T extends Comparable<T>> void mergeSort(T[] A, T[] temp, int l, int r, int k)
@@ -14,43 +14,34 @@ public class ModifiedMergeSort {
 			return;
 		
 		int L = r - l + 1;
-		int newL = l;
-		int currMid = 0;
-		int inc = Math.max(L / k + 1, 1);
+		int remainder = L % k;
+		int greatestMul = L - remainder;
+		int nEl = L / k;
+		int newl = l;
+		int mid;
 		
-		if (L % k == 0) 
-			inc = 1;
-		
-		int[] indices;
-		
-		if (L < k) {
-			int mid = (l + r) / 2;
-			
-			indices = new int[2];
-			System.out.println("_mergeSort(A, temp, " + l +", " + (mid - 1) + ", k);");
-			System.out.println("_mergeSort(A, temp, " + mid +", " + r + ", k);");
-			indices[0] = l;
-			indices[1] = mid;
-//			mergeSort(A, temp, l, mid - 1, k);
-//			mergeSort(A, temp, mid, r, k);
-		} 
-		else 
+		// use regular merge sort
+		if (L < k)
 		{
-			indices = new int[k];
-			// if k = 3, L = 8, then i runs from 1 to 3
+			mid = (l + r) / 2;
+			System.out.println("_m(A, t, " + l + ", " + mid + ", k=" + k + ")");
+			System.out.println("_m(A, t, " + (mid + 1) + ", " + r + ", k=" + k + ")");
+		}
+		// use modified merge sort
+		else
+		{
 			for (int i = 0; i < k; i++)
 			{
-				// so it doesn't index out of bounds
-				currMid = Math.min(currMid + inc, r);
+				// the last ones house L / k + 1 more element
+				if (i == k - remainder)
+					mid = newl + nEl;
+				// the first ones house L / k elements
+				else
+					mid = newl + nEl - 1;
 				
-				// so no element is missed at the end
-				if (i == k -1)
-					currMid = r + 1;
-				
-				System.out.println("mergeSort(A, temp, " + newL +", " + (currMid - 1) + ", k);");
-//				mergeSort(A, temp, newL, currMid - 1, k);
-				indices[i] = newL;
-				newL = currMid;
+				System.out.println("m(A, t, " + newl + ", " + mid + ", k=" + k + ")");
+	//			mergeSort(A, temp, newl, mid, k);
+				newl = mid + 1;
 			}
 		}
 		
