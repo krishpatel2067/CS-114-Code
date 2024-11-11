7da80205409fdc25b573592e73a1674a9c2e1ba8
Krish A. Patel <krishpatel2067@gmail.com>
2024-11-11 09:20:10 -0500

    Format document & write the print statements for all the available sorting methods

--- Lab10/src/lab10/MovieRecord.java
+++ Lab10/src/lab10/MovieRecord.java
@@ -1,32 +1,42 @@
 /* Starter code given by the professor */
 package lab10;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class MovieRecord implements Comparable<MovieRecord> {
-    private String title;
-    private int rating;
+	private String title;
+	private int rating;
 
-    MovieRecord(String t, int r) {
-        title = t;
-        rating = r;
-    }
+	MovieRecord(String t, int r) {
+		title = t;
+		rating = r;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public int getRating() {
-        return rating;
-    }
+	public int getRating() {
+		return rating;
+	}
 
-    public String toString() {
-        String s = title+"\t"+rating;
-        return s;
-    }
+	public String toString() {
+		String s = title + "\t" + rating;
+		return s;
+	}
 
+	// purely based on ratings
     public int compareTo(MovieRecord m) {
         return this.rating - m.rating;
     }
+
+    /* Written by Krish A. Patel */
+	// also take into account title alphabetic order
+//	public int compareTo(MovieRecord m)
+//	{
+//		int diff = this.rating - m.rating;
+//		return diff == 0 ? -m.title.compareTo(this.title) : diff;
+//	}
 
 }
--- Lab10/src/lab10/SortMovies.java
+++ Lab10/src/lab10/SortMovies.java
@@ -1,5 +1,6 @@
 /* Starter code given by the professor */
 package lab10;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,67 +11,133 @@ import java.util.ArrayList;
 
 public class SortMovies {
 
-    public static void main(String[] args) throws IOException {
-        File ratings = new File("movies.tsv");
-        ArrayList<MovieRecord> mr = new ArrayList<MovieRecord>();
+	public static void main(String[] args) throws IOException {
+		File ratings = new File("movies.tsv");
+		ArrayList<MovieRecord> mr = new ArrayList<MovieRecord>();
 
-        int count = 0;
+		int count = 0;
 
-        try {
-            Scanner scanr = new Scanner(ratings);
-            while (scanr.hasNextLine()) {
-                String line = scanr.nextLine();
-                String[] tokens = line.split("\\t"); // Input separated by tabs
-                MovieRecord mrec = new MovieRecord(tokens[0], Integer.parseInt(tokens[1]));
-                mr.add(mrec);
-            }
-            scanr.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+		try {
+			Scanner scanr = new Scanner(ratings);
+			while (scanr.hasNextLine()) {
+				String line = scanr.nextLine();
+				String[] tokens = line.split("\\t"); // Input separated by tabs
+				MovieRecord mrec = new MovieRecord(tokens[0], Integer.parseInt(tokens[1]));
+				mr.add(mrec);
+			}
+			scanr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
-        System.out.println("Number of rating records= "+mr.size());
-        MovieRecord[] A = mr.toArray(new MovieRecord[mr.size()]);
+		System.out.println("Number of rating records= " + mr.size());
+		MovieRecord[] A = mr.toArray(new MovieRecord[mr.size()]);
 
-        // Use the following auxilliary array for mergesort or counting sort.
-        MovieRecord[] B = new MovieRecord[A.length];
+		// Use the following auxilliary array for mergesort or counting sort.
+		MovieRecord[] B = new MovieRecord[A.length];
 
-        Sorts srts = new Sorts();
+		Sorts srts = new Sorts();
 
-        long start = System.currentTimeMillis();
+		/* Written by Krish A. Patel */
+		// --------------------------------------------------------------------------------------
 
-        // Insert code here for sorting array.
+		// Insert code here for sorting array.
+		int sampStart = (int) (B.length / 2.5);
+		int sampEnd = (int) (B.length / 2.4999);
+		
+		sampStart = 0;
+		sampEnd = 50;
 
+		// counting sort
+		long start = System.currentTimeMillis();
+		countingsort(A, B, 100);
+		long end = System.currentTimeMillis();
 
-        long end = System.currentTimeMillis();
-        System.out.println("Time = "+(end-start));
+		System.out.print("Counting (STABLE) sort finished: ");
+		System.out.println("Time = " + (end - start));
+		prt(B, sampStart, sampEnd, "\t");
+		System.out.println();
 
-        // Print out the sorted records.
+		// merge sort
+		B = new MovieRecord[A.length];
+		start = System.currentTimeMillis();
+		srts.mergesort(A, B, 0, B.length - 1);
+		end = System.currentTimeMillis();
 
+		System.out.print("Merge (STABLE) sort finished: ");
+		System.out.println("Time = " + (end - start));
+		prt(B, sampStart, sampEnd, "\t");
+		System.out.println();
 
-        BufferedWriter output = new BufferedWriter(new FileWriter("smovies.tsv"));
-        // Write the output to a file; depending on sort, output A or B.
-        for(int i=0; i<A.length; ++i)
-            output.write(String.format("%s\n", A[i]));
+		// heap sort
+		B = A.clone();
+		start = System.currentTimeMillis();
+		srts.heapsort(B);
+		end = System.currentTimeMillis();
 
-        output.close();
+		System.out.print("Heap (UNSTABLE) sort finished: ");
+		System.out.println("Time = " + (end - start));
+		prt(B, sampStart, sampEnd, "\t");
+		System.out.println();
+		
+//      // quick sort (STACK OVERFLOW)
+//      B = A.clone();
+//      start = System.currentTimeMillis();
+//      srts.quicksort(B);
+//      end = System.currentTimeMillis();
+//      
+//      System.out.print("Quick sort finished: ");
+//      System.out.println("Time = "+(end-start));
+//      prt(B, sampStart, sampEnd, "\t");
+//      System.out.println();
+		
+//		// hybrid quick sort (STACK OVERFLOW)
+//		B = A.clone();
+//		start = System.currentTimeMillis();
+//		srts.hqsort(B, 0, B.length - 1);
+//		end = System.currentTimeMillis();
+//
+//		System.out.print("Hybrid quick sort finished: ");
+//		System.out.println("Time = " + (end - start));
+//		prt(B, sampStart, sampEnd, "\t");
+//		System.out.println();
 
-    }
+		// Print out the sorted records.
 
+		// --------------------------------------------------------------------------------------
 
-    /*
-     * counting sort from the notes, specialized for MovieRecords.
-     * Assumes the ratings are between 0 and bound.
-     * The sorted output goes into B, an array that we create and pass in.
-    */
-    static void countingsort(MovieRecord[] A, MovieRecord[] B, int bound) {
-        int[] C = new int[bound+1];
-        for(int i=0; i< bound+1; ++i) C[i] = 0;
-        for(int j=0; j< A.length; ++j) C[A[j].getRating()]++;
-        for(int i=1; i< bound+1; ++i) C[i] = C[i]+C[i-1];
-        for(int j=A.length-1; j>= 0; --j) {
-            B[C[A[j].getRating()]-1] = A[j];
-            --C[A[j].getRating()];
-        }
-    }
+		BufferedWriter output = new BufferedWriter(new FileWriter("smovies.tsv"));
+		// Write the output to a file; depending on sort, output A or B.
+		for (int i = 0; i < A.length; ++i)
+			output.write(String.format("%s\n", A[i]));
+
+		output.close();
+
+	}
+
+	static <T> void prt(T[] arr, int s, int e, String prefix) {
+		System.out.println("Sample size: " + (e - s));
+
+		for (int i = s; i < e; i++)
+			System.out.println(prefix + arr[i]);
+	}
+
+	/*
+	 * counting sort from the notes, specialized for MovieRecords. Assumes the
+	 * ratings are between 0 and bound. The sorted output goes into B, an array that
+	 * we create and pass in.
+	 */
+	static void countingsort(MovieRecord[] A, MovieRecord[] B, int bound) {
+		int[] C = new int[bound + 1];
+		for (int i = 0; i < bound + 1; ++i)
+			C[i] = 0;
+		for (int j = 0; j < A.length; ++j)
+			C[A[j].getRating()]++;
+		for (int i = 1; i < bound + 1; ++i)
+			C[i] = C[i] + C[i - 1];
+		for (int j = A.length - 1; j >= 0; --j) {
+			B[C[A[j].getRating()] - 1] = A[j];
+			--C[A[j].getRating()];
+		}
+	}
 }
