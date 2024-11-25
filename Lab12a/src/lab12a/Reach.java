1fb478353bef7ccfad552d86818f91e9bafac78f
Krish A. Patel <krishpatel2067@gmail.com>
2024-11-25 08:45:11 -0500

    Setup Lab12a

---
+++ Lab12a/src/lab12a/Reach.java
@@ -0,0 +1,71 @@
+package lab12a;
+
+import java.util.LinkedList;
+import java.util.Random;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+//import java.util.Collections;
+
+
+public class Reach {
+
+    static int dd;
+
+    public static void main(String[] args) {
+
+        int n;
+
+        /*
+         * We don't know in advance the size of the graph, so let's keep
+         * track of the edges as we read them in; we will add them to the
+         * graph later.
+         */
+
+        ArrayList<Integer> From = new ArrayList<Integer>();
+        ArrayList<Integer> To = new ArrayList<Integer>();
+
+        int max = 0; // Maximum vertex number so we know
+                     // how large a graph to build.
+        File file = new File("web.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextInt()) {
+                int src = scanner.nextInt();
+                int dst = scanner.nextInt();
+                if(src>max) max=src;
+                if(dst>max) max=dst;
+                From.add(src);
+                To.add(dst);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("max= "+max+" From size= "+From.size());
+        Graphl graph = new Graphl(max+1);
+
+        n = max;
+        for(int i=0; i<From.size(); ++i) {
+            graph.setEdge(From.get(i), To.get(i),1);
+        }
+        System.out.println("Built graph");
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter source and destination:");
+            int s = input.nextInt();
+            int d = input.nextInt();
+            if((s<0) || (d<0)) break;
+
+
+            // Add code to determine if d is reachable from s.
+
+
+        }
+        input.close();
+    }
+
+}
